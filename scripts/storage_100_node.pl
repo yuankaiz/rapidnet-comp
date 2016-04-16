2bed599f622741ba1f14d35e8edc1a49a46bf9bd
Chen Chen <chenche@cis.upenn.edu>
2016-04-16 11:46:44 -0400

    Add a new graph for storage

--- scripts/storage_100_node.pl
+++ scripts/storage_100_node.pl
@@ -1,11 +1,12 @@
 set terminal pdf
-set output "prov_storage.pdf"
+set output "storage_100n_10p_200pk_3plots.pdf"
 
-set title "100-switch topology with 20 communicating pairs"
+set title "100-switch topology with 20 pairs of hosts, each sending 200 packets"
 
 set xlabel "Storage Size (MB)"
 set ylabel "Percentage of nodes (%)"
 
 set key right bottom
-plot [0:30] "/home/chen/research/rapidnet-comp/scripts/storage_cdf_100_20_nocomp.dat" using ($1/1000000):2 title "no compression" with lines,\
-     "/home/chen/research/rapidnet-comp/scripts/storage_cdf_100_20_online.dat" using ($1/1000000):2 title "online compression" with lines
+plot [0:30] "/home/chen/research/rapidnet-comp/scripts/storage_cdf_100n_10p_200pk_nocomp.dat" using ($1/1000000):2 title "no compression" with lines,\
+     "/home/chen/research/rapidnet-comp/scripts/storage_cdf_100n_10p_200pk_online.dat" using ($1/1000000):2 title "online compression" with lines,\
+     "/home/chen/research/rapidnet-comp/scripts/storage_cdf_100n_10p_200pk_strawman.dat" using ($1/1000000):2 title "strawman compression" with lines
