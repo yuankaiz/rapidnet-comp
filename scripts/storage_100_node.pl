6e2368b019d3ba997805146a3b4520faf9e04ce7
Chen Chen <chenche@cis.upenn.edu>
2016-04-16 20:20:27 -0400

    Gerate storage graph for normal packet forwarding

--- scripts/storage_100_node.pl
+++ scripts/storage_100_node.pl
@@ -1,12 +1,12 @@
 set terminal pdf
-set output "storage_100n_10p_200pk_3plots.pdf"
+set output "storage_norm_100n_10p_200pk_3plots.pdf"
 
-set title "100-switch topology with 20 pairs of hosts, each sending 200 packets"
+set title "100-switch topology with 10 pairs of hosts, each sending 1000 packets"
 
 set xlabel "Storage Size (MB)"
 set ylabel "Percentage of nodes (%)"
 
 set key right bottom
-plot [0:30] "/home/chen/research/rapidnet-comp/scripts/storage_cdf_100n_10p_200pk_nocomp.dat" using ($1/1000000):2 title "no compression" with lines,\
-     "/home/chen/research/rapidnet-comp/scripts/storage_cdf_100n_10p_200pk_online.dat" using ($1/1000000):2 title "online compression" with lines,\
-     "/home/chen/research/rapidnet-comp/scripts/storage_cdf_100n_10p_200pk_strawman.dat" using ($1/1000000):2 title "strawman compression" with lines
+plot "/home/chen/research/rapidnet-comp/scripts/storage_cdf_norm_100n_10p_1000pk_nocomp.dat" using ($1/1000000):2 title "no compression" with lines,\
+     "/home/chen/research/rapidnet-comp/scripts/storage_cdf_norm_100n_10p_1000pk_online.dat" using ($1/1000000):2 title "online compression" with lines,\
+     "/home/chen/research/rapidnet-comp/scripts/storage_cdf_norm_100n_10p_1000pk_strawman.dat" using ($1/1000000):2 title "strawman compression" with lines
