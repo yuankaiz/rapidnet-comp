0c81bad725d4bfa159dd95894836bad633587b3c
Chen Chen <chenche@cis.upenn.edu>
2016-05-04 15:32:30 -0400

    Add the storage growth graph for node pairs from 100 to 1000

--- scripts/gnuplot_scripts/scale_100_node.pl
+++ scripts/gnuplot_scripts/scale_100_node.pl
@@ -1,13 +1,12 @@
 set terminal pdf
-set output "storage_growth_norm_100n_1000pk_3plots.pdf"
+set output "storage_growth_100n_1000pkts_100to1000_3plots.pdf"
 
 set title "Storage growth with the number of communicating pairs (100-node topology, 1000 packets)"
 
-set xlabel "Number of nodes"
+set xlabel "Number of node pairs"
 set ylabel "Average storage (MB)"
 
 set key left top
-plot "/home/chen/research/rapidnet-comp/expr_data/prov_compress/storage_growth_nocomp_100n_1000pkts_10to90.dat" using 1:($2/1000000) title "no compression" with lines,\
-     "/home/chen/research/rapidnet-comp/expr_data/prov_compress/storage_growth_onlinenew_100n_1000pkts_10to90.dat" using 1:($2/1000000) title "online compression" with lines,\
-     "/home/chen/research/rapidnet-comp/expr_data/prov_compress/storage_growth_strawmannew_100n_1000pkts_10to90.dat" using 1:($2/1000000) title "strawman compression" with lines
-
+plot "/home/chen/research/rapidnet-comp/expr_data/prov_compress/storage_growth_nocomp_100n_1000pkts_100to1000.dat" using 1:($2/1000000) title "no compression" with lines,\
+     "/home/chen/research/rapidnet-comp/expr_data/prov_compress/storage_growth_onlinenew_100n_1000pkts_100to1000.dat" using 1:($2/1000000) title "online compression" with lines,\
+     "/home/chen/research/rapidnet-comp/expr_data/prov_compress/storage_growth_strawman_100n_1000pkts_100to1000.dat" using 1:($2/1000000) title "strawman compression" with lines
