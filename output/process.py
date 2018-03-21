c291e982fa2b41854ae418b9c8131b1578f5131a
yuankai <yz345@georgetown.edu>
2018-03-20 23:27:20 -0400

    output processing

--- output/process.py
+++ output/process.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 import os
-storageinfo = os.stat('1')
+storageinfo = os.stat('prov/1')
 print "Provenance tables size: " + str(storageinfo.st_size) + " bytes"
 
 class Record(object):
@@ -10,28 +10,41 @@ class Record(object):
         self.timing = int(textline.split()[-1])
         self.iid = int(textline[textline.find(':')+1:textline.find(')')])
 
-with open('timing', 'r') as f:
-    lines = [line.rstrip('\n') for line in f]
-
-iData = {}
-rData = defaultdict(list)
+proviData = {}
+provrData = defaultdict(list)
 # iid: [records of iid]
-output = {}
+provoutput = {}
 # iid: avg time took for each image
 
-for l in lines:
-    record = Record(l)
-    if record.isImage:
-        iData[record.iid] = record
-    else:
-        rData[record.iid].append(record)
+mliData = {}
+mlrData = defaultdict(list)
+# iid: [records of iid]
+mloutput = {}
+# iid: avg time took for each image
 
-for i, r in iData.iteritems():
-    n = total = 0
-    for res in rData[i]:
-        n += 1
-        total += res.timing - r.timing
-    output[i] = total/n
+def timing_analysis(timing_file, iData, rData, output):
+    with open(timing_file, 'r') as f:
+        lines = [line.rstrip('\n') for line in f]
 
-for i, o in output.iteritems():
-    print 'image ' + str(i) + ' has ' + str(len(rData[i]))  + ' results, took average ' + str(o) + ' microseconds'
+    for l in lines:
+        record = Record(l)
+        if record.isImage:
+            iData[record.iid] = record
+        else:
+            rData[record.iid].append(record)
+
+    for i, r in iData.iteritems():
+        n = total = 0
+        for res in rData[i]:
+            n += 1
+            total += res.timing - r.timing
+        output[i] = total*1.0/n
+
+timing_analysis("prov/timing", proviData, provrData, provoutput)
+timing_analysis("ml/timing", mliData, mlrData, mloutput)
+
+for i, o in provoutput.iteritems():
+    ptime = o
+    mtime = mloutput[i]
+    increase = (ptime - mtime) * 1.0 / mtime
+    print 'image ' + str(i) + ' ml-only: ' + str(mtime) + ', prov: ' + str(ptime) + ', overhead: ' + str(increase*100) + '%'
